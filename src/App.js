3bff6eb4a3ea422b0c0a7d9288aca56eb3c2dcd7
Blake Wood Jr <blake@rewired.one>
2022-06-15 15:33:32 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import NodeWalletConnect from "@walletconnect/node";
+import WalletConnectQRCodeModal from "@walletconnect/qrcode-modal";
+
+// Create connector
+const walletConnector = new NodeWalletConnect(
+  {
+    bridge: "https://bridge.walletconnect.org", // Required
+  },
+  {
+    clientMeta: {
+      description: "WalletConnect NodeJS Client",
+      url: "https://nodejs.org/en/",
+      icons: ["https://nodejs.org/static/images/logo.svg"],
+      name: "WalletConnect",
+    },
+  }
+);
 
 function App() {
+
+  const connect = () => {
+    if (!walletConnector.connected) {
+      // create new session
+      walletConnector.createSession().then(() => {
+        // get uri for QR Code modal
+        const uri = walletConnector.uri;
+        // display QR Code modal
+        WalletConnectQRCodeModal.open(
+          uri,
+          () => {
+            console.log("QR Code Modal closed");
+          },
+          true // isNode = true
+        );
+      });
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => connect()} style={{cursor: 'pointer'}}>Wallet Connect</button>
     </div>
   );
 }
